9cd6955b999ba61420520c167a90c40af84f89dc
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-12-10 15:57:20 +0530

    Add files via upload

---
+++ imageMatch.js
@@ -0,0 +1,106 @@
+const cards = document.querySelectorAll('.image-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let card1, card2;
+
+function cardFlip() {
+  if (lockBoard) return;
+  if (this === card1) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    card1 = this;
+
+    return;
+  }
+
+  // second click
+  card2 = this;
+  var value=document.getElementById('trials').innerHTML;
+  var new_count=(value.charAt(value.length - 1)-'0') - 1;
+
+
+  var new_value=value.substring(0, value.length - 1)+new_count;
+
+
+
+  document.getElementById('trials').innerHTML=new_value;
+  match_check();
+
+
+  if(checkfortrials())
+  {
+    alert("Game Over");
+    window.location.reload();
+  }
+}
+
+
+
+function checkfortrials()
+{
+  var value=document.getElementById('trials').innerHTML;
+  var count=value.charAt(value.length - 1)-'0';
+  console.log(count==0);
+  return count==0;
+
+
+}
+
+function match_check() {
+  let isMatch = card1.dataset.framework === card2.dataset.framework;
+
+  isMatch ? disableCards() : uncardFlips();
+}
+
+function disableCards()
+
+{
+  card1.removeEventListener('click', cardFlip);
+  card2.removeEventListener('click', cardFlip);
+
+  reset();
+}
+
+function uncardFlips()
+
+ {
+  lockBoard = true;
+
+  setTimeout(() =>
+
+   {
+    card1.classList.remove('flip');
+    card2.classList.remove('flip');
+
+    reset();
+  }, 1000);
+
+
+}
+
+function reset()
+
+
+{
+  [hasFlippedCard, lockBoard] = [false, false];
+  [card1, card2] = [null, null];
+}
+
+(
+  function shuffle()
+
+{
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+
+
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', cardFlip));
